0f420ebec5709724615dbdcf18058e9077d01768
GoldenLilies <aimer219@hotmail.com>
2023-12-04 14:56:57 +0100

    index.php change to make login work

--- index.php
+++ index.php
@@ -12,3 +12,30 @@
 </body>
 
 </html>
+
+<?php
+// Include (require) files here, cookies config etc.
+
+// Include the appropriate content based on the login status
+session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+ }
+
+if (isset($_SESSION['user_id'])) {
+    // User is logged in, include dashboard content
+    include('dashboard.inc.php');
+    //include('bookingtable.inc.php');
+} else {
+    // User is not logged in, include login content
+    include('login.inc.php');
+}
+
+if (isset($_GET['logout'])) {
+    echo 'Du er nå logget ut.';
+}
+
+if (isset($_GET['registered'])) {
+    echo 'Profilen din er opprettet. Vennligst logg inn.';
+}
+?>
