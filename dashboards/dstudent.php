1f617b1f4d8958be2667587a127d207efe91dc4a
GoldenLilies <aimer219@hotmail.com>
2023-12-04 18:58:43 +0100

    dstudent with correct require path

--- dashboards/dstudent.php
+++ dashboards/dstudent.php
@@ -3,7 +3,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-require_once('../../phpbooking/db.php');
+require_once(__DIR__ . '/../db.php');
 
 /* Debug statements
 echo '<pre>';
