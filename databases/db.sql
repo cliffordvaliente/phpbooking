fba59339a7b37dc2eb84e7c4f48fdf30da9f73b4
Cliffey Valiente <cliffeyv@massimocliffords-macbook.local>
2023-12-04 23:12:24 +0100

    x

---
+++ databases/db.sql
@@ -0,0 +1,70 @@
+/* ________________________________________________________
+ 
+ HOW TO LOGIN VIA TERMINAL IN MACOS MAMP
+ 
+ / Applications / MAMP / Library / bin / mysql - uroot - p
+ 
+ PASSWORD: ROOT 
+ 
+ ___________________________________________________________
+ */
+CREATE TABLE
+  Users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('Teacher', 'Student', 'Assistant') NOT NULL,
+    experience TEXT,
+    specialization VARCHAR(255),
+    availability TEXT
+  );
+
+
+/* 
+ DROPPED 
+ */
+CREATE TABLE
+  Courses (
+    course_id INT AUTO_INCREMENT PRIMARY KEY,
+    course_name VARCHAR(255) NOT NULL,
+    description TEXT
+  );
+
+
+CREATE TABLE
+  Guidance_Sessions (
+    session_id INT AUTO_INCREMENT PRIMARY KEY,
+    teacher_id INT,
+    course_id INT,
+    length INT,
+    session_theme VARCHAR(255),
+    FOREIGN KEY (teacher_id) REFERENCES Users(user_id) ON DELETE
+    SET
+      NULL,
+      FOREIGN KEY (course_id) REFERENCES Courses(course_id) ON DELETE
+    SET
+      NULL
+  );
+
+
+CREATE TABLE
+  Messages (
+    message_id INT AUTO_INCREMENT PRIMARY KEY,
+    sender_id INT NOT NULL,
+    recipient_id INT NOT NULL,
+    message_text TEXT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (recipient_id) REFERENCES Users(user_id) ON DELETE CASCADE
+  );
+
+
+CREATE TABLE
+  Student_Courses (
+    student_id INT,
+    course_id INT,
+    PRIMARY KEY (student_id, course_id),
+    FOREIGN KEY (student_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (course_id) REFERENCES Courses(course_id) ON DELETE CASCADE
+  );
--- register.php
+++ register.php
@@ -1,99 +1,110 @@
 <?php
 
-require_once('db.php');
+//! INCLUDE PHP FILES HERE 
+include_once 'functions.php';
+hidestatus();
 
-/* This is the registration code*/
+//! DB STUFF 
+require_once('databases/db.php');
+$errorMessages = [];
 
- $errormessage = array();
+//! USER REGISTRATION STATEMENTS CONDITIONS
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-// Below is the user registration. It checks against what is wrong and not against what is correct, by using $error.
- if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $firstname = trim($_POST["firstname"]);
-    $lastname = trim($_POST["lastname"]);
-    $cellphone = trim($_POST["cellphone"]);
+    //! FULL NAME
+    $full_name = trim($_POST["full_name"]);
+    //! EMAIL
     $email = trim($_POST["email"]);
+    //! USERS PASSWORD
     $password = trim($_POST["password"]);
     $password_repeat = trim($_POST["password_repeat"]);
-    $selectedCourses = isset($_POST["courses"]) ? $_POST["courses"] : [];
+    //! COURSES
+    $selectedCourses = isset($_POST["course_id"]) ? $_POST["courses"] : [];
     $courses = implode(', ', $selectedCourses);
 
-    if (empty($firstname)) {
-        $error[] = "Fornavn må fylles ut";
-    }
-    if (empty($lastname)) {
-        $error[] = "Etternavn må fylles ut";
-    }
-    if (empty($cellphone)) {
-        $error[] = "Mobilnummer må fylles ut";
-    } elseif (!preg_match('/^[49]/', $cellphone) || !is_numeric($cellphone) || strlen($cellphone) !== 8) {
-        $error[] = "Mobilnummer må være et 8-sifret gyldig norsk mobilnummer.";
+    //! All the input must not be empty
+    if (empty($full_name)) {
+        $error[] = "Fult Navn må fylles ut";
     }
     if (empty($email)) {
         $error[] = "Email må fylles ut";
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    }
+    //! Check the format is EMAIL
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $error[] = "Emailformatet er ugyldig";
     }
+
+    //! COURSES
     if (empty($selectedCourses)) {
         $error[] = "Husk å velge faget eller fagene dine!";
     }
     if (empty($password)) {
         $error[] = "Passordfeltet må fylles ut";
-    } elseif (strlen($password) < 8 || !preg_match('/[A-ZÆØÅ]/', $password) || !preg_match('/[0-9]/', $password)) {
+    }
+    //! CHECK THE PASSWORD MEETS CRITERIA 8 length and special char
+    elseif (strlen($password) < 8 || !preg_match('/[A-ZÆØÅ]/', $password) || !preg_match('/[0-9]/', $password)) {
         $error[] = "Passordet må være minst 8 tegn langt, inneholde minst én stor bokstav og ett tall. Vi anbefaler at du har noen små bokstaver også.";
     } else {
+        //! MAKE PASSWORD SECURED
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
     }
     if ($password !== $password_repeat) {
+        //! PASSWORD CHECKER IS CORRECT WRITTEN
         $error[] = "Pass på at du gjentar passordet likt";
     }
-    if (empty($error)) {
+    //! IF ALL STATEMENTS HAVE NO ERROR THIS LINE RUNS
+  
+      if (empty($errorMessages)) {
         include('insert.inc.php');
-        // Redirect to index.php after successful registration
+        //! REDIRECT TO INDEX
         header("Location: index.php?registered=1");
-        exit(); // No further code runs after this one runs
+        exit();
+    } else {
+        foreach ($errorMessages as $message) {
+            echo "<p>Error: $message</p>";
+        }
     }
+}
 
-    // Here, each error is stored and then displayed on screen as a separate line.
+    //* Here, each ERROR is stored and then displayed on screen as a separate line.
     else {
         echo "Feil oppstod, vennligst trykk på tilbakeknappen og rett på følgende:<br>";
         foreach ($error as $errormessage) {
             echo "-$errormessage<br>";
-        };
+        }
+        ;
     }
- }
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="no">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrer en bruker</title>
 </head>
+
 <body>
-    <!-- HTML-form for user registration -->
-    <h2>Brukerregistrering</h2>
-    <form method="post" action="" accept-charset="UTF-8">
+
+    <h3>Brukerregistrering</h3>
+    <form method="POST" action="insert.inc.php" accept-charset="UTF-8">
         <label for="firstname">Fornavn:</label>
-        <input type="text" name="firstname" value="<?php echo isset($firstname) ? htmlspecialchars($firstname) : ''; ?>"><br>
-
-        <label for="lastname">Etternavn:</label>
-        <input type="text" name="lastname" value="<?php echo isset($lastname) ? htmlspecialchars($lastname) : ''; ?>"><br>
-
-        <label for="cellphone">Mobilnummer:</label>
-        <input type="text" name="cellphone" value="<?php echo isset($cellphone) ? htmlspecialchars($cellphone) : ''; ?>"><br>
+        <input type="text" name="full_name"
+            value="<?php echo isset($full_name) ? htmlspecialchars($full_name) : ''; ?>"><br>
 
         <label for="email">Email:</label>
         <input type="text" name="email" value="<?php echo isset($email) ? htmlspecialchars($email) : ''; ?>"><br><br>
-    
-        <label>Fagkode:</label><br>
-        <input type="checkbox" name="courses[]" value="IS-116"> IS-116<br>
-        <input type="checkbox" name="courses[]" value="IS-115"> IS-115<br>
-        <input type="checkbox" name="courses[]" value="ORG-128"> ORG-128<br><br>
 
-        <p>Passordet må være minst 8 tegn langt, inneholde <b>minst</b> én stor bokstav og ett tall. Vi anbefaler at du har noen små bokstaver også.</p>
+        <label>Fagkode</label><br>
+        <input type="checkbox" name="courses[]" value="2"> IS-115<br>
+        <input type="checkbox" name="courses[]" value="1"> IS-116<br>
+
+
+        <p>Passordet må være minst 8 tegn langt, inneholde <b>minst</b> én stor bokstav og ett tall. Vi anbefaler at du
+            har noen små bokstaver også.</p>
 
         <label for="password">Passord:</label>
         <input type="password" name="password"><br>
@@ -105,4 +116,5 @@ require_once('db.php');
     </form>
 
 </body>
+
 </html>
