72832b1e8f25945bf8ce95c4029375a145bdd970
Cliffey Valiente <cliffeyv@massimocliffords-macbook.local>
2023-12-04 19:12:14 +0100

    x

--- login.php
+++ login.php
@@ -1,47 +1,55 @@
 <?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
- }
-require_once('db.php');
-// include cookies
-// Check if the user is already logged in
-//session_start(); // SET COOKIES HERE
 
+//! INCLUDE PHP FILES HERE 
+include 'functions.php';
+hidestatus();
+
+//! DB STUFF 
+require_once('db.php');
+
+//! IF USER LOGGED IN -> REDIRECT TO DASHBOARD
 if (isset($_SESSION['user_id'])) {
-    // User is already logged in, redirect to dashboard
     header("Location: dashboards/dstudent.php");
     exit();
 }
 
+//! STRING TO WRITE THE ERROR MESSAGE
 $errormessage = '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     $email = trim($_POST["email"]);
     $password = trim($_POST["password"]);
     $stmt = $pdo->prepare("SELECT user_id, email, password, role FROM users WHERE email = ?");
+
     $stmt->execute([$email]);
 
-    // Check if the user exists before attempting to fetch data
+    //! CHECK DB IF USER EXIST IN TABLE BEFORE FETCH DATA
     if ($stmt->rowCount() > 0) {
+
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
-        // Verify the password
+
+        //! PASSWORD CHECK
         if (password_verify($password, $user['password'])) {
-            // Login successful, set session variables and redirect internally to dashboard include
+
+
+            //! SET SESSION VARIABLES WHEN SUCCESSFUL AND REDIRECT TO DASHBOARD
             $_SESSION['user_id'] = $user['user_id'];
             $_SESSION['role'] = $user['role'];
             //header('Location: dashboard.inc.php');
             include('dashboards/dstudent.php');
             exit();
         } else {
-            $errormessage = "Feil brukernavn eller passord."; // Error message for wrong email or password
+            //! GET ERROR MESSAGE IF WRONG CREDENTIALS
+            $errormessage = "Feil brukernavn eller passord.";
             error_log("Login Error: Incorrect email or password for $email", 0);
         }
     } else {
-        $errormessage = "Feil brukernavn eller passord."; // Error message for account which doesnt exist, same text
+        //! ERROR MESSAGE IF EMAIL NOT FOUND IN TABLE DB
+        $errormessage = "Feil brukernavn eller passord.";
         error_log("Login Error: Email not found for $email", 0);
     }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -52,22 +60,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logg inn</title>
 </head>
+
 <body>
     <h1>Bookingapplikasjon </h1>
     <h2>Logg inn</h2>
-        <form method="post" action="index.php">
-            <label for="email">E-post:</label>
-            <input type="email" name="email" required>
+    <form method="post" action="index.php">
+        <label for="email">E-post:</label>
+        <input type="email" name="email" required>
 
-            <br>
+        <br>
 
-            <label for="password">Passord:</label>
-            <input type="password" name="password" required>
+        <label for="password">Passord:</label>
+        <input type="password" name="password" required>
 
-            <br>
+        <br>
 
-            <input type="submit" value="Logg inn">
-        </form>
+        <input type="submit" value="Logg inn">
+    </form>
 
     <?php
     if (!empty($errormessage)) {
@@ -76,4 +85,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     ?>
     <p>Ikke registrert ennå? <a href="register.php">Registrer deg her</a></p>
 </body>
+
 </html>
