a652a913a09d461896c25ed228ab90f13f2f44f5
Cliffey Valiente <cliffeyv@massimocliffords-macbook.local>
2023-12-05 14:52:57 +0100

    other

--- files/insert.php
+++ files/insert.php
@@ -6,6 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = filter_var(trim($_POST["email"]), FILTER_VALIDATE_EMAIL);
     $password = trim($_POST["password"]);
     $selectedCourses = isset($_POST["courses"]) ? $_POST["courses"] : [];
+    $additional_info = ''; // Assuming you have additional info for students
 
     if (!$email) {
         // Handle invalid email
@@ -17,10 +18,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
         $pdo->beginTransaction();
 
+        // Insert into Users
         $userSql = "INSERT INTO Users (full_name, email, password, role) VALUES (?, ?, ?, 'student')";
         $pdo->prepare($userSql)->execute([$full_name, $email, $hashed_password]);
         $user_id = $pdo->lastInsertId();
 
+        // Insert into Students
+        $studentSql = "INSERT INTO Students (user_id, additional_info) VALUES (?, ?)";
+        $pdo->prepare($studentSql)->execute([$user_id, $additional_info]);
+
+        // Insert into Student_Courses
         foreach ($selectedCourses as $course_id) {
             $courseSql = "INSERT INTO Student_Courses (student_id, course_id) VALUES (?, ?)";
             $pdo->prepare($courseSql)->execute([$user_id, $course_id]);
